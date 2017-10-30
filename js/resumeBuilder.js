8fc2ea113fe18e40b30f183819c5e05435ddfae8
Georgios Karanikas <georgios.karanikas@crealytics.de>
2017-10-30 15:31:54 +0100

    First bio inserted

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,73 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+ var skills = ["awesomeness", "programming", "teaching", "JS"];
+ var bio = {
+ 	"name": "Jojos",
+ 	"role": "Software Engineer",
+ 	"contacts": {
+ 		"mobile": "+49 176 2797 6680",
+ 		"email": "karanikasg@gmail.com",
+ 		"github": "mcjojos",
+ 		"location": "Berlin"
+ 	},
+ 	"welcomeMessage": "Something cheerful",
+ 	"age": 35,
+ 	"skills": skills,
+ 	"bioPic": "images/die_mauer.jpg"
+ };
+
+var education = {
+	"schools" : [
+	{
+		"name": "KTH",
+		"city": "Stockholm",
+		"degree": "Masters",
+		"majors": [Internetworking],
+		"dates": 2005,
+		"url": "www.kth.se"
+	},
+	{
+		"name": "University of Crete",
+		"city": "Heraklion, Crete",
+		"degree": "Bachelors",
+		"majors": [CS],
+		"dates": 2000,
+		"url": "www.uoc.gr"		
+	}
+	],
+	"onlineCourses": [
+	{
+		"title": "Javascript crush course",
+		"school": "Udacity",
+		"dates": 2017,
+		"url": "www.udacity.com/course/ud804"
+	}
+	]
+};
+
+var work = {
+	"jobs": [
+	{
+		"employer": "crealytics",
+		"title": "Senior Software Engineer",
+		"dates": "August 2017 - Future",
+		"description": "Software development made easy"
+	}
+	]
+}
+
+var projects = {
+	"projects": [
+	{
+		"title": "sample project 1",
+		"dates": 2015,
+		"description": "sample description for sample project 1",
+		"images": [
+		"http://piweb.azureedge.net/articles/articlesshare/i/2013/project-puzzle-pieces.jpg",
+		"https://newsignature.com/wp-content/uploads/2017/02/project-management-1024x512.png
+		]
+	}
+	]
+}
+
